c418af4225bc81dc9c6fe47542778c0868905ee9
Lobbbey <joshuatylerbandy@gmail.com>
2025-03-24 21:55:06 -0400

    fully installed tailwind?

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./api/SignUp.php",
+        "./index.html",
+        // Add other file As we create them
+       ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
