143bccb6540660895da2e25e7d599e960b4bda3c
Dylan Katchen <89935431+dylankatchen@users.noreply.github.com>
2025-04-09 04:39:44 -0400

    adjusted signup so that values are properly retunred

--- api/User/signup.php
+++ api/User/signup.php
@@ -1,5 +1,5 @@
 <?php
-    //Variables taken from js file
+    // Variables taken from js file
     $inData = getRequestInfo();
     $Name = $inData["Name"];
     $Email = $inData["Email"];
@@ -8,51 +8,69 @@
     $University = $inData["University"];
 
     $conn = new mysqli("localhost", "APIUser", "Password", "EventManagement");
-    if($conn->connect_error){
+    if ($conn->connect_error) {
         returnWithError("error: Could not connect to database");
-    }
-    else{
-        // Translate the University name into its University ID 
+    } else {
+        // Translate the University name into its University ID
         $uniStmt = $conn->prepare("SELECT University_ID FROM University WHERE Name = ?");
         $uniStmt->bind_param("s", $University);
         $uniStmt->execute();
         $uniResult = $uniStmt->get_result();
         
-        //Returns if username already exists in database
+        if ($uniResult->num_rows === 0) {
+            returnWithError("error: University not found");
+            $uniStmt->close();
+            $conn->close();
+            exit();
+        }
+
+        $University_ID = $uniResult->fetch_assoc()["University_ID"];
+        $uniStmt->close();
+
+        // Check if the user already exists
         $stmt = $conn->prepare("SELECT * FROM Users WHERE Email=?");
         $stmt->bind_param("s", $Email);
         $stmt->execute();
         $result = $stmt->get_result();
 
-        if($row = $result->fetch_assoc()){
+        if ($result->fetch_assoc()) {
             returnWithError("error: User Already Exists");
-        }
-        else{
-            //Insert user into database
-            $University_ID = $uniResult->fetch_assoc()["University_ID"];
-            $stmt = $conn->prepare("INSERT INTO Users (Name, Email, Password, UserType, University_ID) Values (?,?,?,?,?)");
+        } else {
+            // Insert user into the database
+            $stmt = $conn->prepare("INSERT INTO Users (Name, Email, Password, UserType, University_ID) VALUES (?, ?, ?, ?, ?)");
             $stmt->bind_param("ssssi", $Name, $Email, $Password, $Usertype, $University_ID);
             $stmt->execute();
-            sendResultInfoAsJson('{"result":"Finished Successfully"}');
+
+            // Fetch the newly created user's UID
+            $UID = $stmt->insert_id;
+
+            // Return the required data
+            $response = [
+                "UID" => $UID,
+                "Name" => $Name,
+                "UserType" => $Usertype,
+                "University_ID" => $University_ID
+            ];
+            sendResultInfoAsJson(json_encode($response));
         }
+
         $stmt->close();
-        $uniStmt->close();
         $conn->close();
     }
 
-    //Gets input from file in key-value pairs
-    function getRequestInfo(){
+    // Gets input from file in key-value pairs
+    function getRequestInfo() {
         return json_decode(file_get_contents('php://input'), true);
     }
 
-    //Returns JSON as error message
-    function returnWithError($err){
-        $retvalue = '{"result":"' . $err . '"}';
+    // Returns JSON as error message
+    function returnWithError($err) {
+        $retvalue = '{"error":"' . $err . '"}';
         sendResultInfoAsJson($retvalue);
     }
     
-    //Returns JSON Object
-    function sendResultInfoAsJson($obj){
+    // Returns JSON Object
+    function sendResultInfoAsJson($obj) {
         header('Content-type: application/json');
         echo $obj;
     }
